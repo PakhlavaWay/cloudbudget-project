8d5ff4f1c399659d5f706f2ef3e886dcff698b81
ShZh <zhorobaev0707@gmail.com>
2022-09-16 16:33:46 +0600

    'first-layout'

---
+++ index.js
@@ -0,0 +1,55 @@
+// ----------------- swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: "cards",
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true
+  },
+});
+
+// ---------------------- play button
+
+const playButton = document.querySelector(".software__play-button");
+playButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".software__video").play();
+  playButton.style.display = "none";
+})
+
+// --------------------- burger menu
+
+const burger = document.querySelector(".header__burger");
+const rightSide = document.querySelector(".header__right-side");
+const links = document.querySelectorAll(".header__link");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  rightSide.classList.toggle("active");
+})
+
+links.forEach( link => {
+  link.addEventListener("click", (e)  => {
+    e.preventDefault();
+    burger.classList.remove("active");
+    rightSide.classList.remove("active");
+  })
+})
+
+// ----------------------- accordion 
+
+const accordionBox = document.querySelectorAll(".accordion__box");
+
+accordionBox.forEach(box => {
+  box.addEventListener("click", (e) => {
+    e.preventDefault();
+    box.classList.toggle("active");
+  })
+})
+  
